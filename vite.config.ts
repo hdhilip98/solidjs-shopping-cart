21012009bd41d5be258cb1bf0aaf9e12f0854d26
hdhilip98 <dhiliph98@outlook.com>
2022-06-30 22:33:59 +0530

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
+  base: "/solidjs-shopping-cart/",
+  publicDir: "./public",
   plugins: [solidPlugin()],
   build: {
     target: "esnext",
     polyfillDynamicImport: false,
   },
-  publicDir: "./public",
-  base: "/solidjs-shopping-cart",
 });
