4e62a4fc14e308e8cd73b5404a46b16293399ccc
hdhilip98 <dhiliph98@outlook.com>
2022-06-30 23:07:24 +0530

    add content to about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,21 @@
+import { Anchor, Flex, Text } from "@hope-ui/solid";
+
 const About = () => {
   return (
-    <div>
-      <h2>About</h2>
-    </div>
+    <Flex direction="column" gap="$4">
+      <Text fontSize="$2xl">Solidjs Shopping Cart</Text>
+      <Text>
+        A simple shopping cart with{" "}
+        <Anchor external href="https://www.solidjs.com/" color="$info9">
+          Solid.js
+        </Anchor>{" "}
+        and{" "}
+        <Anchor external href="https://hope-ui.com/" color="$info9">
+          Hope UI
+        </Anchor>
+        .
+      </Text>
+    </Flex>
   );
 };
 
