99aaee54058027a287101811ec1c544f091b35a3
hdhilip98 <dhiliph98@outlook.com>
2022-06-30 22:41:45 +0530

    remove gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
-  base: "/solidjs-shopping-cart/",
   publicDir: "./public",
   plugins: [solidPlugin()],
   build: {
